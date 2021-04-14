66ecd4e48b85a8d437c1475ff2e88b3ac9b0bf88
saikiran0505 <saikiran.arangi05@gmail.com>
2021-04-14 14:39:55 +0530

    mybook.java testing

---
+++ MyBook.java
@@ -0,0 +1,23 @@
+public class MyBook
+{
+public static void main (String args[])
+{
+String Bookname = "classic";
+Short Bookprice = 700;
+String colour = "yellow";
+Short pages= 1000;
+String author ="saikiran";
+Byte rating = 5;
+
+System.out.println(Bookname);
+System.out.println(Bookprice);
+System.out.println(colour);
+System.out.println(pages);
+System.out.println(author);
+System.out.println(rating);
+}
+}
+
+
+
+
