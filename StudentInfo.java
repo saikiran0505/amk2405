ab620dd640c0ea9d2095b08fbea6f29e0c865e0c
saikiran0505 <saikiran.arangi05@gmail.com>
2021-04-14 14:41:12 +0530

    studentinfo.java

---
+++ StudentInfo.java
@@ -0,0 +1,16 @@
+public class StudentInfo
+{
+public static void main (String args[])
+{
+System.out.println("name : sai");
+System.out.println("age : 25");
+System.out.println("email: sai.arangi@gmail.com");
+System.out.println("name : sai");
+System.out.println("contact no : 7799551151");
+System.out.println("local address: hyderabad");
+}
+}
+
+
+
+
