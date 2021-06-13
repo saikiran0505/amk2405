f28bdc4239813759fbdb115f9b11dc130dbbdd01
saikiran0505 <80593665+saikiran0505@users.noreply.github.com>
2021-06-13 14:31:12 +0530

    Add files via upload

---
+++ BookInfo.java
@@ -0,0 +1,23 @@
+public class BookInfo
+{
+public static void main (String args[])
+{
+String bn = "classic";
+Short bp = 700;
+String bc = "yellow";
+Short bpag= 1000;
+String bau ="saikiran";
+Byte br = 5;
+
+System.out.println("Bookname : "+bn);
+System.out.println("Bookprice : "+bp);
+System.out.println("colour : "+bc);
+System.out.println("pages : "+bpag);
+System.out.println("author : "+bau);
+System.out.println("rating : "+br);
+}
+}
+
+
+
+
